5e86f7de3ea3103a0926c2c3f606ecdd934cc710
Filipe Rodrigues <filipe.r26@gmail.com>
2025-03-30 17:56:19 -0300

    Agenda 05 - Menus de Contexto e Selecionando um Item na ListView

--- MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -49,8 +49,6 @@ public partial class ListaProduto : ContentPage
         {
             string q = e.NewTextValue;
 
-            lst_produtos.IsRefreshing = true;
-
             lista.Clear();
 
             List<Produto> tmp = await App.Db.Search(q);
@@ -60,10 +58,6 @@ public partial class ListaProduto : ContentPage
         catch (Exception ex)
         {
             await DisplayAlert("Ops", ex.Message, "OK");
-        }
-        finally
-        {
-            lst_produtos.IsRefreshing = false;
         }
     }
 
@@ -113,27 +107,6 @@ public partial class ListaProduto : ContentPage
         catch (Exception ex)
         {
             DisplayAlert("Ops", ex.Message, "OK");
-        }
-    }
-
-    private async void lst_produtos_Refreshing(object sender, EventArgs e)
-    {
-        try
-        {
-            lista.Clear();
-
-            List<Produto> tmp = await App.Db.GetAll();
-
-            tmp.ForEach(i => lista.Add(i));
-        }
-        catch (Exception ex)
-        {
-            await DisplayAlert("Ops", ex.Message, "OK");
-
-        }
-        finally
-        {
-            lst_produtos.IsRefreshing = false;
         }
     }
 }
